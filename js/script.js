2e74255ac030db8d123eac9b2e3344ab58546b03
sabrina-fontana <sabfon@hotmail.it>
2020-12-14 15:28:53 +0100

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var kmNumber = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+
+var age = parseInt(prompt('Quanti anni hai?'));
+
+var ticketPrice = kmNumber * 0.21;
+
+var text = document.getElementById('ticket-price');
+
+if (isNaN(kmNumber) || isNaN(age)) {
+  alert('Uno o più valori inseriti non sono corretti. Riprova.');
+} else {
+  if (age < 18) {
+    text.innerHTML = ('Il prezzo del tuo biglietto è di € ' + (ticketPrice * 0.8).toFixed(2))
+  } else if (age > 65) {
+    text.innerHTML = ('Il prezzo del tuo biglietto è di € ' + (ticketPrice * 0.6).toFixed(2))
+  } else {
+    text.innerHTML = ('Il prezzo del tuo biglietto è di € ' + ticketPrice.toFixed(2))
+  }
+}
